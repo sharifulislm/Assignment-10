ff1fa0d151ed4258d8aeaffc47755207f5950372
sharifulislm <shariful.islam.coder@gmail.com>
2022-04-18 13:05:23 +0300

    added techor components

--- src/conponents/Homepage/HomePage.js
+++ src/conponents/Homepage/HomePage.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Courses from '../Courses/Courses';
 import Home from '../Home/Home';
 import KidsService from '../KidsService/KidsService';
+import Teacher from '../Teacher/Teacher';
 
 
 const HomePage = () => {
     return (
         <div>
             <Home></Home>
+         
             <Courses></Courses>
+            <Teacher></Teacher>
             <KidsService></KidsService>
+            
 
             
         </div>
